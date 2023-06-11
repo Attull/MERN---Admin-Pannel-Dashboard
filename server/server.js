7626544ea1c1cb5b73b36ddec1b225cd63ae3cb8
Attull <atul852798@gmail.com>
2023-06-11 14:45:44 +0530

    add dotenv file

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 import express from  "express"
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
+const PORT = process.env.PORT || 5000
 
-
-app.get('/',()=>{
-    console.log("Server started")
+app.get('/',(req, res)=>{
+    res.send("Server started")
 })
 
-app.listen(3000, ()=>{
-    console.log("listen to the PORT 3000")
+app.listen(PORT, ()=>{
+    console.log(`listen to the PORT ${PORT}`)
 })
